1c81188154982e3172d8aa398ddbfa2a0776b7b7
Greg Matthew Crossley <greg@neomindlabs.com>
2022-11-10 12:44:51 -0500

    Better handle a nil search

--- spec/nppes_api_spec.rb
+++ spec/nppes_api_spec.rb
@@ -7,4 +7,7 @@ describe NPPESApi do
   it 'successfully searches for a provider' do
     expect(NPPESApi.search(number: 1932494937).results.first.taxonomies.first.state).to eq('NC')
   end
+  it 'gracefully handles a nil search' do
+    expect(NPPESApi.search(number: 00000000).results).to be_nil
+  end
 end
